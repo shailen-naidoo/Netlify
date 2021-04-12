c6c92e5c54de56963f61a7b6052bf22e37d6de38
David Hayes <david.hayes@asd20.org>
2021-04-12 12:46:34 -0600

    feat: support env variable substitution

--- src/config.ts
+++ src/config.ts
@@ -7,10 +7,23 @@ interface BuildStatusColors {
   error: string;
 }
 
-const siteId = vscode.workspace.getConfiguration('netlify').get('site_id') as string;
-const apiToken = vscode.workspace.getConfiguration('netlify').get('api_token') as string;
+function substituteEnvVariables(input: string): string {
+  let configString = JSON.stringify(input);
+
+  if (configString && /\${env:[^}]+}/.test(configString)) {
+    const { groups } = configString.match(/\${env:(?<name>[^}]+)\}/) as RegExpMatchArray;
+
+    if (groups?.name && process.env[groups.name]) {
+      configString = configString.replace(/(\${env:[^}]+})/g, process.env[groups.name] as string);
+    }
+  }
+  return configString
+}
+
+const siteId = substituteEnvVariables(vscode.workspace.getConfiguration('netlify').get('site_id') as string);
+const apiToken = substituteEnvVariables(vscode.workspace.getConfiguration('netlify').get('api_token') as string);
 const setInterval = vscode.workspace.getConfiguration('netlify').get('set_interval') as number;
-const buildHook = vscode.workspace.getConfiguration('netlify').get('build_hook') as string;
+const buildHook = substituteEnvVariables(vscode.workspace.getConfiguration('netlify').get('build_hook') as string);
 const buildStatusColors = vscode.workspace.getConfiguration('netlify').get('build_status_colors') as BuildStatusColors;
 
 export {
